8994acfed57fc76bc11006e5e70c61e65f671a88
ASLawan <nawalnitsua@gmail.com>
2024-08-16 05:13:53 +0100

    Sort comppleted todos

--- toDoApp/src/components/ToDoList.jsx
+++ toDoApp/src/components/ToDoList.jsx
@@ -2,9 +2,15 @@ import ToDoItem from "./ToDoItem";
 import styles from "./todolist.module.css";
 
 export default function ToDoList({ todos, setTodos }) {
+  // Sort todos
+
+  const sortedTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
+
   return (
     <div className={styles.listcontainer}>
-      {todos.map((item) => (
+      {sortedTodos.map((item) => (
         <ToDoItem
           key={item.name}
           item={item}
